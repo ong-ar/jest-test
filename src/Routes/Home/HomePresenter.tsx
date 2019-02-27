b5cf205057ffd9b89954eae47e88988cd79b7671
ong-ar <reclusis@gmail.com>
2019-02-27 11:42:20 +0900

    add test component

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
+import Button from "../../Components/Button";
 
-const HomePresenter: React.SFC = () => <div>test</div>;
+const HomePresenter: React.SFC = () => (
+  <div>
+    <Button title="dddddd" />
+  </div>
+);
 
 export default HomePresenter;
